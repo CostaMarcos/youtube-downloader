6328543a3e3f8750ded282570ad3e87fb3268457
Marcos Vinicius <marcosvinicius.mecatronica@gmail.com>
2022-10-07 19:53:30 -0400

    versao 1

---
+++ codigo/YoutubeDownload.py
@@ -0,0 +1,11 @@
+from pytube import YouTube
+from pytube.cli import on_progress
+
+link = input("Insira o link do vídeo: ")
+yt = YouTube(link, on_progress_callback = on_progress)
+print("Video: ", yt.title)
+print("Iniciando o download, aguarde alguns instantes...")
+
+ys = yt.streams.get_highest_resolution()
+ys.download(output_path=".")
+input()
